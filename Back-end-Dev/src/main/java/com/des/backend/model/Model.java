4f514a35e05920a09555e648d09ed74ca2f87e22
fearaujo293 <fearaujo487@gmail.com>
2025-03-31 08:37:36 -0300

    Update Model.java

--- Back-end-Dev/src/main/java/com/des/backend/model/Model.java
+++ Back-end-Dev/src/main/java/com/des/backend/model/Model.java
@@ -1,5 +1,20 @@
 package com.des.backend.model;
 
 public class Model {
-    
+
+     private String conteudo;
+
+    public Mensagem(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+}
+
 }
