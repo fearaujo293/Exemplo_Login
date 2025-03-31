9795729818a1f73f663e53ae804ff5fd7da31156
fearaujo293 <fearaujo487@gmail.com>
2025-03-31 08:39:11 -0300

    Update Service.java

--- Back-end-Dev/src/main/java/com/des/backend/service/Service.java
+++ Back-end-Dev/src/main/java/com/des/backend/service/Service.java
@@ -1,4 +1,15 @@
 package com.des.backend.service;
 
+import com.exemplo.demo.model.Mensagem;
+import org.springframework.stereotype.Service;
+
+@Service
 public class Service {
+  
+  public Mensagem obterMensagem() {
+        return new Mensagem("Olá do back-end em Java!");
+    }
+
+
 }
+
