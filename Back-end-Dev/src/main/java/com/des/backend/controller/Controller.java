86785d8bc4fc6dcc9c658b966d894bd293766cf2
fearaujo293 <fearaujo487@gmail.com>
2025-03-31 08:36:50 -0300

    Update Controller.java

--- Back-end-Dev/src/main/java/com/des/backend/controller/Controller.java
+++ Back-end-Dev/src/main/java/com/des/backend/controller/Controller.java
@@ -1,4 +1,20 @@
 package com.des.backend.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
 public class Controller {
+   
+  @GetMapping("/mensagem")
+  public Map<String, String> getMensagem() {
+        return Map.of("mensagem", "Olá do back-end em Java!");
+    }
+}
+
+  
 }
